825ac80728766e20382f27ed07fbfee70444cc38
Jonathan <jon.chua51@gmail.com>
2022-12-17 20:39:28 -0800

    connected login and registration to service

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,44 +1,62 @@
-import React from 'react'
-import './RegisterStyles.css'
-
+import React, { useState } from 'react';
+import Axios from 'axios';
+import server from '../../config/service';
+import './RegisterStyles.css';
 
 const Register = () => {
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const register = () => {
+        Axios.post(`${server.local.host}/v1/account/register`, {
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            username: username,
+            password: password
+        }).then((res) => {
+            console.log(res.data);
+        });
+    }
+
     return (
         <div class="register-box">
             <h2>Create an Account</h2>
             <form>
                 <div class="user-box">
-                    <input type="firstName" name="" required=""/>
+                    <input type="firstName" name="" required="" onChange={(e) => { setFirstname(e.target.value); }}/>
                     <label>First Name</label>
                 </div>
 
                 <div class="user-box">
-                    <input type="lastName" name="" required=""/>
+                    <input type="lastName" name="" required="" onChange={(e) => { setLastname(e.target.value); }}/>
                     <label>Last Name</label>
                 </div>
 
                 <div class="user-box">
-                    <input type="userEmail" name="" required=""/>
+                    <input type="userEmail" name="" required="" onChange={(e) => { setEmail(e.target.value); }}/>
                     <label>E-Mail</label>
                 </div>
 
                 <div class="user-box">
-                    <input type="username" name="" required=""/>
+                    <input type="username" name="" required="" onChange={(e) => { setUsername(e.target.value); }}/>
                     <label>Username</label>
                 </div>
 
                 <div class="user-box">
-                    <input type="password" name="" required=""/>
+                    <input type="password" name="" required="" onChange={(e) => { setPassword(e.target.value); }}/>
                     <label>Password</label>
                 </div>
-
-                    <a href="/signin">
+                    <button onClick={register}>
                     <span></span>
                     <span></span>
                     <span></span>
                     <span></span>
                     Register
-                    </a>
+                    </button>
             </form>
         </div>
 
--- src/components/signin/SignIn.jsx
+++ src/components/signin/SignIn.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
-import './SignInStyles.css'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react';
+import Axios from 'axios';
+import server from '../../config/service';
+import './SignInStyles.css';
+import {Link} from 'react-router-dom';
 
 
 const SignIn = () => {
+    const [username, getUsername] = useState("");
+    const [password, getPassword] = useState("");
+
+    const login = () => {
+        Axios.get(`${server.local.host}/v1/account/login`, {
+            auth: {
+                username: username,
+                password: password
+            }
+        }).then((res) => {
+            console.log(res.data);
+        });
+    }
     return (
         <div class="signin-box">
             <h2>Login</h2>
             <form>
                 <div class="user-box">
-                    <input type="username" name="" required=""/>
+                    <input type="username" name="" required="" onChange={(e) => { getUsername(e.target.value);  }}/>
                     <label>Username</label>
                 </div>
 
                 <div class="user-box">
-                    <input type="password" name="" required=""/>
+                    <input type="password" name="" required="" onChange={(e) => { getPassword(e.target.value);  }}/>
                     <label>Password</label>
                 </div>
 
-                    <a href="/">
+                    <button onClick={login}>
                     <span></span>
                     <span></span>
                     <span></span>
                     <span></span>
                     SignIn
-                </a>
+                </button>
 
                 <div className="class-box">
                     <h5>Don't have an Account? Register with us today!</h5>
