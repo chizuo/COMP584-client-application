d4c3ffd97b06436c110f4119ddc408d4de76633c
AyeAronnnn <aaron.gl.madrid@gmail.com>
2022-12-19 03:17:09 -0800

    Forgot this

--- src/components/forum/Forum.jsx
+++ src/components/forum/Forum.jsx
@@ -8,7 +8,7 @@ const Forum = () => {
             <div className="container">
                 <div className="content">
                     <h2>Talk about <span> Bikes</span></h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, illo. Quisquam, ipsam sunt repudiandae vero ullam consequatur commodi aliquam rem dolorem quidem! Amet atque, fugiat dicta deleniti officia quod quibusdam!</p>
+                    <p>Biking is a great way to get exercise and explore the outdoors. Whether you prefer to ride on trails, roads, or city streets, there is a bike for every type of terrain and rider!</p>
                     <button><a href="/postbike" className="postbike-page">Post Your Own Bike Here</a></button>
                 </div>
             </div>
